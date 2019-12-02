605eaf0862218c78f4ae8efed2eb3e5ee6029d0e
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2019-12-01 18:45:23 -0600

    finish the second puzzle

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -2,7 +2,31 @@ use std::fs::File;
 use std::io::Read;
 
 pub fn calculate_fuel(mass: usize) -> usize {
-    (mass / 3) - 2
+    let fuel_requirement = mass / 3;
+
+    if fuel_requirement < 2 {
+        return 0;
+    }
+
+    fuel_requirement - 2
+}
+
+// What I should actually do is solve the equation, but this matches the specification in the
+// advent challenge and I suspect is less accurate in a way that would effect what the advent
+// considers correct. Instead I'll have to implement what was written.
+pub fn recursive_fuel_cost(mass: usize) -> usize {
+    let mut new_mass = mass;
+    let mut total_fuel_mass = 0;
+
+    loop {
+        let fuel_mass = calculate_fuel(new_mass);
+        if fuel_mass == 0 { break; }
+
+        total_fuel_mass += fuel_mass;
+        new_mass = fuel_mass;
+    }
+
+    total_fuel_mass
 }
 
 fn main() {
@@ -11,8 +35,13 @@ fn main() {
 
     in_dat_fh.read_to_string(&mut in_dat).unwrap();
 
-    let result: usize = in_dat.lines().map(|i| calculate_fuel(i.parse::<usize>().unwrap())).sum();
-    println!("Fuel required: {}", result);
+    let input_masses: Vec<usize> = in_dat.lines().map(|i| i.parse::<usize>().unwrap()).collect();
+
+    let first_result: usize = input_masses.iter().map(|i| calculate_fuel(*i)).sum();
+    println!("Fuel required: {}", first_result);
+
+    let second_result: usize = input_masses.iter().map(|i| recursive_fuel_cost(*i)).sum();
+    println!("Recursive fuel calculation: {}", second_result);
 }
 
 #[cfg(test)]
@@ -25,5 +54,12 @@ mod test {
         assert_eq!(calculate_fuel(14), 2);
         assert_eq!(calculate_fuel(1969), 654);
         assert_eq!(calculate_fuel(100756), 33583);
+    }
+
+    #[test]
+    fn test_recursive_fuel_calculations() {
+        assert_eq!(recursive_fuel_cost(12), 2);
+        assert_eq!(recursive_fuel_cost(1969), 966);
+        assert_eq!(recursive_fuel_cost(100756), 50346);
     }
 }
