ebe6638a683ed41911254eaab5917608c675c1c4
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2019-12-02 18:48:52 -0600

    add in the extra programs for additional validation

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -344,4 +344,21 @@ mod test {
         assert_eq!(ic.run(), Ok(()));
         assert_eq!(ic.memory_str(), "3500,9,10,70,2,3,11,0,99,30,40,50");
     }
+
+    #[test]
+    fn test_additional_progs() {
+        let programs: [(&'static str, &'static str); 4] = [
+            ("1,0,0,0,99", "2,0,0,0,99"),
+            ("2,3,0,3,99", "2,3,0,6,99"),
+            ("2,4,4,5,99,0", "2,4,4,5,99,9801"),
+            ("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99"),
+        ];
+
+        for (prog, result) in programs.iter() {
+            let mut ic = IntcodeComputer::from_str(prog).unwrap();
+
+            assert_eq!(ic.run(), Ok(()));
+            assert_eq!(ic.memory_str(), result.to_string());
+        }
+    }
 }
